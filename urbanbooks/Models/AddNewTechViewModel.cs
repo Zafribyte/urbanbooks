1e775c5491c9bd07f6376d29c7374cf4e133ab1a
Darren <s209202195@nmmu.ac.za>
2014-08-22 10:23:56 +0200

    fix

---
+++ urbanbooks/Models/AddNewTechViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace urbanbooks.Models
+{
+    public class AddNewTechViewModel
+    {
+    }
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace urbanbooks.Models
+{
+    public class AddNewTechViewModel
+    {
+        public List<SelectListItem> techCategories { get; set; }
+        public List<SelectListItem> suppliers { get; set; }
+        public List<SelectListItem> manufacturers { get; set; }
+        public IEnumerable<TechCategory> techCategory { get; set; }
+        public IEnumerable<Manufacturer> manufacturer { get; set; }
+        public IEnumerable<Technology> tech { get; set; }
+        public Technology techs { get; set; }
+    }
+}
