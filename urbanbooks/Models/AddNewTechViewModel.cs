33d73cd9ac04849214cabf82dd87ebb0375cadc0
Gcobani Poswa <s210139889@live.nmmu.ac.za>
2014-08-22 11:46:17 +0200

    techFix

--- urbanbooks/Models/AddNewTechViewModel.cs
+++ urbanbooks/Models/AddNewTechViewModel.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace urbanbooks.Models
-{
-    public class AddNewTechViewModel
-    {
-    }
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
