44335683313c8baa46b7e7442136853c9f16dea8
Gcobani Poswa <s210139889@live.nmmu.ac.za>
2014-08-25 11:03:24 +0200

    vj

--- urbanbooks/Global.asax.cs
+++ urbanbooks/Global.asax.cs
@@ -5,7 +5,6 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using TwitterBootstrapMVC;
 
 namespace urbanbooks
 {
@@ -16,8 +15,6 @@ namespace urbanbooks
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-            Bootstrap.Configure();
             
             
         }
