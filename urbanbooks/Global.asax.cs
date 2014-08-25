a017de558a9eae6bf53b2ee300e046699d937ba6
Gcobani Poswa <s210139889@live.nmmu.ac.za>
2014-08-25 09:28:36 +0200

    straping in

--- urbanbooks/Global.asax.cs
+++ urbanbooks/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using TwitterBootstrapMVC;
 
 namespace urbanbooks
 {
@@ -16,6 +17,7 @@ namespace urbanbooks
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Bootstrap.Configure();
             
             
         }
