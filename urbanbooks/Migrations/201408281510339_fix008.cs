4622a7b5d7d94bdf5f8ad051699b80e9e5294e78
Gcobani Poswa <s210139889@live.nmmu.ac.za>
2014-08-28 17:11:23 +0200

    migration

---
+++ urbanbooks/Migrations/201408281510339_fix008.cs
@@ -0,0 +1,16 @@
+namespace urbanbooks.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class fix008 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
