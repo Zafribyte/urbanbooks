22715461ae13f20453937d42b7cc559e3e1aa4ee
Gcobani Poswa <s210139889@live.nmmu.ac.za>
2014-09-03 15:51:29 +0200

    SearchViewModel

---
+++ urbanbooks/Models/SearchViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace urbanbooks.Models
+{
+    public class SearchViewModel
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Query { get; set; }
+        [NotMapped]
+        public IEnumerable<Book> BookResults { get; set; }
+        [NotMapped]
+        public IEnumerable<Technology> GadgetResults { get; set; }
+        [NotMapped]
+        public IEnumerable<BookCategory> BookCategoryResults { get; set; }
+        [NotMapped]
+        public IEnumerable<TechCategory> GadgetCategoryResults { get; set; }
+    }
+}
