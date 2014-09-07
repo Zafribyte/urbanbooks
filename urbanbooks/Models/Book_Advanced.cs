e8f8d691b5d66bb28ed612bdf2d8f8bf63bf2336
Gcobani Poswa <s210139889@live.nmmu.ac.za>
2014-09-07 16:31:37 +0200

    Advanced Search init

---
+++ urbanbooks/Models/Book_Advanced.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace urbanbooks.Models
+{
+    public class Book_Advanced
+    {
+        [ScaffoldColumn(false)]
+        [Key]
+        public int Id 
+        { get; set; }
+        [Display(Name = "Device Category")]
+        public bool Category
+        { get; set; }
+        [Display(Name = "Book Title")]
+        public bool BookTitle
+        { get; set; }
+        public bool ISBN
+        { get; set; }
+        public bool Author 
+        { get; set; }
+        public bool Publisher
+        { get; set; }
+        [Display(Name = "Search Term")]
+        public string Query
+        { get; set; }
+        [Display(Name = "From")]
+        public double RangeFrom
+        { get; set; }
+        [Display(Name = "To")]
+        public double RangeTo
+        { get; set; }
+    }
+}
---
+++ urbanbooks/Models/Device_Advanced.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace urbanbooks.Models
+{
+    public class Device_Advanced
+    {
+        [ScaffoldColumn(false)]
+        [Key]
+        public int Id 
+        { get; set; }
+        [Display(Name="Device Category")]
+        public bool Category 
+        { get; set; }
+        [Display(Name="Name")]
+        public bool ModelName 
+        { get; set; }
+        [Display(Name="Model Number")]
+        public bool ModelNumber 
+        { get; set; }
+        public bool Manufacturer 
+        { get; set; }
+        [Display(Name="Search Term")]
+        public string Query 
+        { get; set; }
+        [Display(Name="From")]
+        public double RangeFrom 
+        { get; set; }
+        [Display(Name="To")]
+        public double RangeTo 
+        { get; set; }
+
+    }
+}
