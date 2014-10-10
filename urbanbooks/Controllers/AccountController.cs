5c23f7851200e7a33d5b3c1f1370b1c6c78afc2d
Gcobani <s210139889@nmmu.ac.za>
2014-10-10 12:16:22 +0200

    help done

--- urbanbooks/Controllers/AccountController.cs
+++ urbanbooks/Controllers/AccountController.cs
@@ -178,12 +178,9 @@ namespace urbanbooks.Controllers
                 if (result.Succeeded)
                 {
                     await SignInAsync(user, isPersistent: false);
-                     //For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                     //Send an email with this link
                      string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                      var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                      await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-                     //return Json(new { success = true });
                     return RedirectToAction("Index", "Home");
                 }
                 else
@@ -192,8 +189,6 @@ namespace urbanbooks.Controllers
                 }
             }
 
-            // If we got this far, something failed, redisplay form
-            //return View(model);
             return PartialView(model);
         }
 
--- urbanbooks/Migrations/201409201138226_context.cs
+++ urbanbooks/Migrations/201409201138226_context.cs
@@ -7,20 +7,20 @@ namespace urbanbooks.Migrations
     {
         public override void Up()
         {
-            //CreateTable(
-            //    "dbo.Orders",
-            //    c => new
-            //        {
-            //            OrderNo = c.Int(nullable: false, identity: true),
-            //            DateCreated = c.DateTime(nullable: false),
-            //            DateLastModified = c.DateTime(nullable: false),
-            //            Status = c.Boolean(nullable: false),
-            //            DateSent = c.DateTime(nullable: false),
-            //            SupplierID = c.Int(nullable: false),
-            //            EmployeeID = c.Int(nullable: false),
-            //            InvoiceID = c.Int(nullable: false),
-            //        })
-            //    .PrimaryKey(t => t.OrderNo);
+        //    CreateTable(
+        //        "dbo.Orders",
+        //        c => new
+        //            {
+        //                OrderNo = c.Int(nullable: false, identity: true),
+        //                DateCreated = c.DateTime(nullable: false),
+        //                DateLastModified = c.DateTime(nullable: false),
+        //                Status = c.Boolean(nullable: false),
+        //                DateSent = c.DateTime(nullable: false),
+        //                SupplierID = c.Int(nullable: false),
+        //                EmployeeID = c.Int(nullable: false),
+        //                InvoiceID = c.Int(nullable: false),
+        //            })
+        //        .PrimaryKey(t => t.OrderNo);
             
         }
         
